b17de383be142e92161de652fa5965eea6f0d67d
sionParkv <qkrtldhs321@naver.com>
2022-03-25 10:28:04 +0900

    first upload

--- src/app/list/list.scss.d.ts
+++
@@ -1,4 +0,0 @@
-export const greenText: string;
-export const redText: string;
-export const yellowText: string;
-export const navyText: string;
--- src/app/timer/timer.scss.d.ts
+++
@@ -1,3 +0,0 @@
-export const orangeButton: string;
-export const blueButton: string;
-export const redButton: string;
